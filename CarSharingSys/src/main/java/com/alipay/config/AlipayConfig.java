8b8df5a17c7daa9e0c2e0243343756be6fed3968
yangxh0329 <2458892691@qq.com>
2020-04-09 16:23:08 +0800

    feat：支付宝回调地址使用natapp内网映射地址。

--- CarSharingSys/src/main/java/com/alipay/config/AlipayConfig.java
+++ CarSharingSys/src/main/java/com/alipay/config/AlipayConfig.java
@@ -14,36 +14,54 @@ import java.io.IOException;
  */
 
 public class AlipayConfig {
+
+    /**
+     * natapp内网映射的地址
+     */
+    public static final String NATAPP_URL = "http://rdu5ma.natappfree.cc/";
 	
 //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
-	// 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
-	public static String app_id = "2016092200567084";
-	
-	// 商户私钥，您的PKCS8格式RSA2私钥
-    public static String merchant_private_key = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDORvG1I2V1zuhb6DRuKpz1pHHc58tUHnKOj58wGcfGq26k3ReOuRTKsJONcATPoqkheSo2/FJErr56cSVTOu5g6mmnOkWoGKKtwEVxY55tw68T1s28Sw4ODdCSqBrq/6heacuA7Vy6PTU+YAYULi9z7St3FxJM/kJGVJVLarY4NqDAnE2bZarBxbWc3jrL5SqT/eJ3aqv2cxDByP7qUp8DhVXvFnzvNHO4f4dixdEDO4hSNo+HpolGsTV8oO1VCBNSP3uoBAI/VxI/g8tA+Xpej8QOyAmcfsJKYmB91lMMnO1989x0INftyo5mKAHSJpsUcxqMqejuOMpixSLyEOsrAgMBAAECggEAQlIe/n097s2yeXJEATOkOXpAvaStBAOyE1Hr/ZwcViziKxGGtHWfULfLCQt+EIAc3s1FHF6t2LVSOq3MMWFeKTzU8VZT9QLOswyBhz5fPq1OyhMjHKoI/83DRJPV6759FLXxHyexy5NIXiCjc7ORwDT955E8ahLJnjEtuwy5P4MFDrVHow3LMsjt028Lup0l9FrapYMgf1QezUbmXv4+iWdvqL3HwGI1wr9Pnu+E4//C3F4P+9wuVYNUtqRVX3b51kGeFXP81/7JiFxpcvSQeG7Kw9ZtbJXsvgziy3f5Eun3VJjpz2NHhIUO6a6uEC3ubF0RB6yM+xPtRe8B7CPHCQKBgQDorewMYRqdODQFqdnR//XZr6J+r/vnv9SZ3iWwtcbwsKoA7B4pWoTnDvQCDoKFLmXGK4yZmTuBnIi522ucBZKpicg0Yh1MCoGfX/P3nBNSfJqUfe3uKKjKWEkn9YLWhNJb1aWAiTeigaMomkhi+ZOb7fj6PWPocfxyGnq26E4QZQKBgQDi85gV3+V7F/h89C9kV4htjvksix0ZG5p3NmECr5ZXeMy3MFGCXbwGb60xyXEvxaokzXbFXwErfB2xBtNFk5aW7Ws79grXJ9/2tGIvCiPL8ruemot8PARpZaft96RJF1w5o+3pg+Um5OyQo2ioGwTzYtdqqsmuaJEktbql1L6sTwKBgHbk6EkrFT5hCbQbJoJ4GFgztwSlSEMTcr9tFDVHrXAv1a7C+GaJKoAm0+h5IZiMLNUmS6Hw+scwBW1zAXmoH1kiXnrl57KDa9jUXwWXawgrQ5G9B2c6v4Ult7wDZ+fV1ue2M+uKMjOHqqeErphkj1iOMTstQ2/zTtu/TBVKfxlBAoGBAIy85MZD5yoTblwGjVEbWxYdwHWX+z5zuLFOvpAM1R1/YuwDAbjnac9P98ZVFmS6nk1eQNYamT4fJMZ/5gpSgOMtbf9sD8LnDmMN74keL56s8x5i1jb/fPjjOmU+2dkdrGpuNv5FdZhOBEmZnDALOWG5HL8gFBuc6s0qCQoHSQrPAoGBAIGIAymBS3olF9Dl+YOGW2U6OUGEKtk5OkUU69N4LhEYrRV3/u0FI31WW2ZCO+hRTTdxAto6z9gu7IIVwJRCcrKU7beOIVNB5a/9jUt03HipkJEDTDxsef+smY/Jmw40jguKVE4pcZysn2XodxTpSvOKwsGri63ZTjaT+a78ZIli";
-	
-	// 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-    public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6NYU0CeaF/XoUT5mrRBmC10dmvKbbUeImEPdsPsJNkAkK4NwLjCXJMXWe5mh6yhvY68D3+rueK9sANBxhNQ9exB/Eb10etv40GL9YtxGwe1nSEL14jM9OLpjLNpXXMbwf19aduft5ZfmXZXZCGjZND4Bo2ZT5F6wz3KM/G4u7omOBQKce94mVsiCHA6QEZ+COhc1QFiAumtz5Ay5qRxJdfgm00WnCTSBVk7Uiq+HF7smrP+KyZVDzDmBIGzZXcQlw6aqdI7oeeDOVJx+11vyyx6GfVLAdbv1EDylGVIOPx27H6a1BUNGUt0p9FA9IuxBbp47siAF4Tz+vUzCKAKy4wIDAQAB";
+    /**
+     * 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
+     */
+    public static String app_id = "2016102300746553";
 
-	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String notify_url = "http://localhost:8080/bikesharing/notify_url.jsp";
+    /**
+     * 商户私钥，您的PKCS8格式RSA2私钥
+     */
+    public static String merchant_private_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCFIfJs+ypB/sVUcrbrFYxDr/FaO4Yg1UDyZ1g1Rj2TrTaB2JTYC/N3vfy/HJZ7e+A+fokU0Ms23r0QSk7iz6ApBI1tCqE75jVTF5y86Cn1Wr+FwKGsROfel5z1UNFJlXmB0vv4kYBIha7AhGySVGbBULGFH5tHDw/QwMZPV+No5wMuGhm47X2OcENSqxxcfGfVPwKtCZR+0OlMEYGTvQ66vmFZAtKWSj5I+Pz28pwFu4/mOqtyPS+tP8F/0cjrLiyMExdqlFtAAh+fGZeEbCxSQbYmKKBPvumdjejbet8oFo2fW1zulQUvT+qZJfIQt4bEz9CEoRS964g/E7lVMaArAgMBAAECggEALZGRCPxvWmd5443gTlbxy7lNQzVck6Bquo3lA1cnfIStQ4R+bfSpQTMmb8O5PksIVlu5UAaniAB1dj35AmlOyLtmLQyWY4pIdkh+vao5smF9bKqiq/Rp6Sa78rvW5vjivi0ZUdt7rVcUmZC6GNwKz+Bu4sIOxCPDphBV0JKkpCLvsxTqzAE6SCHW06MUWkVAGxBuD9hdALnGhPEhivo7TryHTJCQU6IcIqgYXYEqnO/k1CSikL5+X69JIyrkykI864++zhxu5pDSKi98ev5qD200/10f2iz7x5PJpG0WrLSis98osHSzSPe4nRL4cN7WE/f0r80wPOXcFO3qYE6RAQKBgQDEhRW28xJi68RyY8HcNNYGzPbTpYGlwDPHgnaung+BMCwhsonKM4D5QY5wDI50e39Edi6j3X7sT2DZphl5XBY4ImjA8hJ5EhSdO+Kg/qu/0i53mJWz5plIRAese9vUayeku1P+7+37bM1TkA2+j66ZxBKQ3mMNwOtN5YebEL3lHQKBgQCtbXAkUH3of0qv0iFmKkd+PoHHrUFNxgXcDOU/1NvcUg9CGlTqBumXghKYibBjFog8DbPCUM4ounlIDPvH1MPg5mqtC7v7tWradLmgN6ig7hkJdOYyQwU+JdrDlWkMooKjCrfLX9LRi/iHdcorbQx/mH/MKPW3/dgjCXTA+yD/5wKBgBybUuHi2FZWA66zHtEzT8SFqZh9izXZmK/+KdCZaaBgpsvw9WXdwXSHOnjNGx+RqqEIoxlvcL5tf6evKh4kbYeWWrFbi5zcKOwKf57384Bk1CbMq14Yv09pt8Q/82wkfQzZyAxgWMA0wl7bpWSL/dqcVCeKd9jUsGwScX79HOdxAoGAFtOauQznJZzrhazQvJPACTvM9S02tTflOYtw+S/ZKGycmYTBvHPgdxKlw/ILh19F2zGqgV8FY5L3t6YN1cWJaqapT9GFkqHUSKCodhAdX+E25mm+issLe5yb6LZXt1QEgdU9T8VzCCW2No4oaALYbEr0NzkE7oy40BPEjEoBk+0CgYEAuRXEVIw/dS2uhbwpwALBKy9UsYdiGlIF2uXiBKgfC+7UDADGGse86r8OTyjTN3G2F0UcqmX/vAP9pmSpFk7B5JSD3wmEP+6yDxG8O6KqI9XhLaTMO5sE/z5zYfrT76LZPIvIL/3/AkTpZpZgS1FO1ACQXm3dJw+SHXxvKyHczdM=";
 
-	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String return_url = "http://localhost:8080/bikesharing/return_url.jsp";
+    /**
+     * 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+     */
+    public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsUIvsRPANoE/lG5RSyFqgs/Oe8AoTbTn8E4WqgF2CP5Oyy7EVw5WHQDtW26O8bC/9FJktVtaW7K/LDTKKA2kr0ZwOnuyVPskVblUGH31O0ETBJ9HFL1nfOqbaHGLPUHlrserZxpT0LhrIyz4QLjqNM07EObKCy16e+8A08gJu6QW5UqPp30aeQg6M5Hv/F9Q7EtlZL264Tz1mi4tOnniPCCPmLnSbvi/3r4ci+IyWGspzeDaW+l3QhIk8Ccp45lusgFj26ZGg822KHN4DPAiGFl8b8bKsOrub0zxKU/tO5Ej7EnkqsmdAjval65xPyjdHmHzeAFIUce+7H6sQGcXowIDAQAB";
 
-	// 签名方式
-	public static String sign_type = "RSA2";
-	
-	// 字符编码格式
-	public static String charset = "utf-8";
-	
-	// 支付宝网关
-	public static String gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
-	
-	// 支付宝网关
-	public static String log_path = "C:\\";
+    /**
+     * 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+     *
+     * 这里是使用natapp的内网映射完成回调
+     */
+    public static String notify_url = NATAPP_URL+"pay/notifyUrl";
 
+    /**
+     * 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+     *
+     * 这里是使用natapp的内网映射完成回调
+     */
+    public static String return_url = NATAPP_URL+"pay/returnUrl";
+
+    // 签名方式
+    public static String sign_type = "RSA2";
+
+    // 字符编码格式
+    public static String charset = "utf-8";
+
+    // 支付宝网关
+    public static String gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
+
+    // 支付宝网关
+    public static String log_path = "D:\\MyWorkSpace\\Idea";
 
 //↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
