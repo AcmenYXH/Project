847fdf4635bdfad9bbb0153bb568af9c34e4d758
yangxh0329 <2458892691@qq.com>
2020-06-01 09:37:48 +0800

    tag：本次版本变动了以下功能。 1、增加用户押金退还功能。 2、修复由“入库时间”导致管理员增加车异常bug。

---
+++ CarSharingSys/src/main/java/com/carss/controller/DepositInfoController.java
@@ -0,0 +1,9 @@
+package com.carss.controller;
+
+/**
+ * @Author: yangxh8
+ * @Date: 2020-04-26 15:21:57
+ * @Description:
+ */
+public class DepositInfoController {
+}
---
+++ CarSharingSys/src/main/java/com/carss/entity/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.carss.entity;
+
+/**
+ * @Author: yangxh8
+ * @Date: 2020-05-26 10:55:34
+ * @Description:
+ */
+public class LoginRequest {
+    
+}
